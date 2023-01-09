2b0d006d5929ccfc2febee7580c5a11003fd16c6
AlexeyKozinets <alexey.kozinets.workmail@gmail.com>
2023-01-09 19:39:50 +0300

    delete all

--- main_site/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import gettext_lazy as _
-
-
-class MainSiteConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'main_site'
-    verbose_name = _('Компании и оборудование')
